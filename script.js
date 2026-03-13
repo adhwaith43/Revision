ebcf1f81fe051bba1f995dd26e7161b4bb55312f
Adhwaith A M <adhwaith43@gmail.com>
2026-03-13 17:57:04 +0530

    commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// console.log("script.js")
+
+
+// console.log("hello","world",3)
+// console.error("error")
+// console.warn("warning")
+
+
+// const c=64
+// console.log(`hi ${c}`)
+
+//  npm init -y
+// npm-package manager used to install different dependencies
