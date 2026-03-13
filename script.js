5424d69958c5641395cdf5cfa3c0f1cd059b4121
Adhwaith A M <adhwaith43@gmail.com>
2026-03-13 18:02:01 +0530

    commit 2

--- script.js
+++ script.js
@@ -9,5 +9,11 @@
 // const c=64
 // console.log(`hi ${c}`)
 
-//  npm init -y
+//  npm init -y --->manage dependencies and versions
 // npm-package manager used to install different dependencies
+
+// const prompt = require("prompt-sync") 
+// require ---> used to import stuffs
+
+// const v = prompt("type something")
+// console.log(v)
